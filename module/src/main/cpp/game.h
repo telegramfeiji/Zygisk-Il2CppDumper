8e34531fa29a58de21a27b784b42482a6d73ea9a
telegramfeiji <130913474+telegramfeiji@users.noreply.github.com>
2023-11-04 14:40:29 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com."
+#define GamePackageName "com.weile.game.buyu"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
